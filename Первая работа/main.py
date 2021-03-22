d86c1a631840b4c2af8e3efbc87dee0c554a5da8
Ureshipan <75073370+Ureshipan@users.noreply.github.com>
2021-03-22 18:06:31 +0300

    Первая работа

---
+++ Первая работа/main.py
@@ -0,0 +1,26 @@
+from flask import Flask
+from data import db_session
+from data.users import User
+from data.jobs import Jobs
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
+
+
+def main():
+    db_session.global_init("db/blogs.db")
+    job = Jobs()
+    job.team_leader = 1
+    job.job = "deployment of residential modules 1 and 2"
+    job.work_size = 15
+    job.collaborators = "2, 3"
+    job.is_finished = False
+    db_sess = db_session.create_session()
+    db_sess.add(user)
+    
+    db_sess.commit()    
+    #app.run()
+
+
+if __name__ == '__main__':
+    main()
